a022ab8c6f349ae1b99e7ef4b25654489c996ebe
Yuyi <yuyi365@gmail.com>
2021-12-14 15:59:08 -0500

    commit tree 9d4dd8a4fc0e4c2bfd7d48ca25089c3339d57b06

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+  wrap_parameters false
+
+  # GET /items
+  def index
+    items = Item.all
+    render json: items, include: :user, status: :ok
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  wrap_parameters false
+
+  #GET /users/:id
+  def show
+    user = User.find(params[:id])
+    render json: user, include: :items, status: :ok
+  end
+
+  private
+
+  def render_not_found
+    render json: { error: 'user not found' }, status: :not_found
+  end
 end
